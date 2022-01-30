0364092b20fd937db8d9e5801bc5ec894bc1d3df
BirdWithDreams <87135024+BirdWithDreams@users.noreply.github.com>
2022-01-30 18:35:31 +0200

    Committing supporting files

--- main.py
+++ main.py
@@ -10,18 +10,14 @@ data = np.array([[1, 1, 1, 0, 0, 0, 0, 0, 0],
 
 ans = np.array([[1, 0], [1, 0], [1, 0], [0, 1], [0, 1], [0, 1]])
 
-MyPerceptron = perc.Perceptron(0.01, 10000, perc.ErrorFunctions.Arctan, 'test')
-MyPerceptron.addLayer(9, MyPerceptron.actFunctions.ReLU)
-MyPerceptron.addLayer(6, MyPerceptron.actFunctions.ReLU)
-MyPerceptron.addLayer(2, MyPerceptron.actFunctions.ReLU)
-
+# MyPerceptron = perc.Perceptron(0.005, 30000, perc.ErrorFunctions.arctan, 'test')
+# MyPerceptron.addLayer(9, MyPerceptron.actFunctions.ReLU)
+# MyPerceptron.addLayer(6, MyPerceptron.actFunctions.ReLU)
+# MyPerceptron.addLayer(2, MyPerceptron.actFunctions.ReLU)
+MyPerceptron = perc.Perceptron.load('test_perceptron.json')
 MyPerceptron.set_data(data, ans)
 MyPerceptron.init()
-MyPerceptron.start()
-MyPerceptron.test_loop()
-MyPerceptron.save()
 
-# p = perc.Perceptron.load("test_perceptron.json")
-# p.set_data(data, ans)
-# p.test_loop()
-# p.a = 0.01
+# MyPerceptron.start()
+MyPerceptron.test_loop()
+# MyPerceptron.save()
